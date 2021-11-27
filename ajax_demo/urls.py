8543bcfa816b07a59c1deb4b3cbe7b70ffce8ab6
ashraf-coder <nyanziashraf1@gmail.com>
2021-11-27 06:12:55 -0800

    pusing changes

--- ajax_demo/urls.py
+++ ajax_demo/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("create_product", views.create_product, name="create_product"),
-    path("ajax_add_product/", views.ajax_add_product, name="ajax_add_product"),
+    path("ajax_add_post/", views.ajax_add_post, name="ajax_add_post"),
 ]
--- ajax_demo/views.py
+++ ajax_demo/views.py
@@ -17,16 +17,17 @@ def create_product(request):
     return render(request, "ajax_demo/create_product.html")
 
 
-def ajax_add_product(request):
+def ajax_add_post(request):
     posted_by = request.POST['posted_by']
     caption = request.POST['caption']
+
     post = Post.objects.create(posted_by=posted_by, caption=caption)
     post.save()
 
-    new_products_count = Post.objects.count()
+    new_post_count = Post.objects.count()
 
     data = {
-        'success_message': "products added successfully",
-        'new_products_count': new_products_count,
+        "success_message": "post added successfully",
+        "new_post_count": new_post_count
     }
     return JsonResponse(data=data, safe=False)
